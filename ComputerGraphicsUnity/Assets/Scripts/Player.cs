817efdbf84a0ec9036540386ae77ed8c8cc5848a
Dylan <SlenderFox@outlook.com>
2019-04-24 15:51:44 +1000

    Finished up the skeletal animations

---
+++ ComputerGraphicsUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    // The speed at which the player moves forward
+    public float speed = 600;
+    // How much force is imparted onto physics objects that are collided into
+    public float pushPower = 15;
+
+    // A reference to the character controller
+    CharacterController controller = null;
+    // A reference to the animator
+    Animator animator = null;
+
+    /// <summary>
+    /// The start function is called once at boot
+    /// </summary>
+    void Start ()
+    {
+        // Properly references the character controller
+        controller = GetComponent<CharacterController>();
+        // Properly references the animator
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update ()
+    {
+        // Gets wasd controls from the input class
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // If the player is not moving forward or backward, revert to idle animation
+        if (vertical == 0)
+            animator.Play("Idle");
+        
+        // Tells the character controller to move the player forward or backward
+        controller.SimpleMove(transform.forward * vertical * speed * Time.deltaTime);
+        // Tells the character controller to rotate the player left or right
+        transform.Rotate(transform.up, horizontal * speed * 0.5f * Time.deltaTime);
+        // Tells the animator to play the walking animation at a set speed
+        animator.SetFloat("Speed", vertical * speed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Is called when a collision is detected
+    /// </summary>
+    /// <param name="hit">The object collided with</param>
+    void OnControllerColliderHit (ControllerColliderHit hit)
+    {
+        // Gets the rigidbody of the object hit, if it does not have one or is kinematic return
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+
+        // If the object hit is already moving away from the player return
+        if (hit.moveDirection.y < -0.3F)
+            return;
+
+        // Applies a push force in the direction the object is moving
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+}
---
+++ ComputerGraphicsUnity/Assets/Scripts/UiScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiScript : MonoBehaviour
+{
+    // The force applied to the cube in the Z axis once it spawns
+    public float m_fThrowForce;
+
+    // A reference to the cube
+    public GameObject m_goCube;
+    // A reference to the empty game object the cube will spawn at
+    public GameObject m_goSpawnPoint;
+
+    /// <summary>
+    /// Spawns a cube at a target location
+    /// </summary>
+    public void SpawnCube()
+    {
+        // Instantiates the cube
+        GameObject newCube = Instantiate(m_goCube, m_goSpawnPoint.transform.position, m_goSpawnPoint.transform.rotation);
+        // Applies force to the cube
+        newCube.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, m_fThrowForce));
+    }
+}
